7412baed2292cbf4c8c31b900cb0d3d6973bdbe5
EpicVerse-app <tech@kriyora.com>
2026-07-06 17:21:49 +0530

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -9,15 +9,9 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
-  const rawNext = searchParams.get('next') // ?? '/dashboard'
+  const rawNext = searchParams.get('next')  ?? '/dashboard'
   // Only allow relative paths to prevent open redirect attacks
-//  const next = rawNext.startsWith('/') ? rawNext : '/dashboard'
-const next =
-  rawNext &&
-  rawNext.startsWith('/') &&
-  !rawNext.startsWith('//')
-    ? rawNext
-    : '/dashboard'
+  const next = rawNext.startsWith('/') ? rawNext : '/dashboard'
   if (code) {
     const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
